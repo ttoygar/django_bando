304c317e61415efcfd6ec5784498288e794ebbc7
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-10-17 02:14:20 +0300

    user authentication/redirect logic created

--- lp/views.py
+++ lp/views.py
@@ -5,4 +5,12 @@ from django.http import HttpResponse
 def landing_page(request):
     # return HttpResponse("Hey")
     # return render(request, "lp/home.html")
-    return redirect('/login/')
+    # permissions = request.user.get_user_permissions()
+
+    # redirects user
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect('/admin/')
+        return render(request, "lp/home.html")
+    else:
+        return redirect('/login/')
