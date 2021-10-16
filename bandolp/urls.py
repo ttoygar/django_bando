1ed57802f157681e6a4e058308eea3e184774b7c
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-10-16 15:43:37 +0300

    created landing page with dummy view

--- bandolp/urls.py
+++ bandolp/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from lp import views as lp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lp_views.landing_page, name='home'),
 ]
--- lp/tests.py
+++ lp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from lp.views import landing_page
+from django.urls import resolve
 
-# Create your tests here.
+
+class LandingTest(TestCase):
+
+    def test_root_resolves_to_landing_page_view(self):
+        page = resolve('/')
+        self.assertEqual(page.func, landing_page)
