ac5663ad7ea567f89e4f062aefa70835536aeef7
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-10-17 04:02:05 +0300

    emails are started to be saved

--- lp/views.py
+++ lp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+from lp.forms import SignUpForm
+
 
 def landing_page(request):
     # redirects user
@@ -14,15 +16,31 @@ def landing_page(request):
 
 
 def signup(request):
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         username = form.cleaned_data.get('username')
+    #         raw_password = form.cleaned_data.get('password1')
+    #         user = authenticate(username=username, password=raw_password)
+    #         login(request, user)
+    #         return redirect('home')
+    # else:
+    #     form = UserCreationForm()
+    # return render(request, 'signup.html', {'form': form})
+
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            # email = form.cleaned_data.get('email')
+            user = authenticate(username=username, password=raw_password) #, email=email)
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
+        # print([field.name for field in form])
+
     return render(request, 'signup.html', {'form': form})
